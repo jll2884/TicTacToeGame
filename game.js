8ee6d70d47624522c3854444d6e732d72541f0c2
jll2884 <simplylopez7@gmail.com>
2019-04-07 16:55:07 -0400

    new changes

--- game.js
+++ game.js
@@ -1,6 +1,12 @@
 let squares= document.querySelectorAll('.tiles');
 let restart = document.getElementById("restart");
 
+let display =  document.getElementById("display");
+
+let playAgain = document.getElementById("again");
+
+let clicks = 0;
+
 let score1 = document.getElementById("score1");
 let score2 = document.getElementById("score2");
 let count1 = 0;
@@ -9,37 +15,84 @@ let player1 = "X";
 let player2 = 'O';
 let player = false;
 
-restart.addEventListener('click',function(){
+// playAgain.style.visibility = 'hidden';
+
+restart.addEventListener('click',function(){ //Resets everything
     for(let i = 0;i<squares.length;i++){
-        squares[i].textContent= ''
+        squares[i].textContent= '';
+        squares[i].classList.remove('avoid-clicks')
 }
+
 player =false;
+count1 = 0;
+count2 = 0;
+score2.textContent= count2;
+score1.textContent= count1;
+display.textContent='';
+clicks=0;
 });
 
+playAgain.addEventListener('click',function(){ //Resets everything
+    for(let i = 0;i<squares.length;i++){
+        squares[i].textContent= ''
+        squares[i].classList.remove('avoid-clicks')
+}
+player =false;
+clicks = 0;
+playAgain.style.visibility = 'hidden';
+restart.style.visibility = 'visible';
+display.textContent = '';
+});
 
+//adds event Listeners to the 
 for(let i = 0;i<squares.length;i++){
     squares[i].addEventListener('click',function () {
-        player = !player
-        let name = ""
+        clicks++;
+
+        player = !player;
+        let name = "";
+        let winner;
         if(player==true){
             this.textContent = player1;
             this.style.color= 'red';
             name = "player 1 has ";
+
         }
         else{
             this.textContent = player2;
             this.style.color= 'blue';
             name = "player 2 has ";
         }
+        this.classList.add("avoid-clicks");
         
-        if(checkRows(this.textContent)) {
-            alert(name+"WON");
+        if(checkRows(this.textContent)==true) {
+            display.textContent= name+"WON!";
+            (this.textContent==player2)? count2++: count1++;
+            (this.textContent==player2)? score2.textContent= " "+count2: score1.textContent= ' '+count1;
+            makeUnclickAble();
+            playAgain.style.visibility = 'visible';
+            restart.style.visibility = 'hidden';
         }
-        else if(checkColumns(this.textContent)){
-            alert(name+"WON");
+        else if(checkColumns(this.textContent)==true){
+            display.textContent= name+"WON!";
+            (this.textContent==player2)? count2++: count1++;
+            (this.textContent==player2)? score2.textContent= ' '+count2: score1.textContent= ' '+count1;
+            makeUnclickAble();
+            playAgain.style.visibility = 'visible';
+            restart.style.visibility = 'hidden';
         }
-        else if(checkAngles(this.textContent)){
-            alert(name+"WON")
+        else if(checkAngles(this.textContent)==true){
+            display.textContent= name+"WON!";
+            (this.textContent==player2)? count2++: count1++;
+            (this.textContent==player2)? score2.textContent= ' '+count2: score1.textContent= ' '+count1;
+            makeUnclickAble();
+            playAgain.style.visibility = 'visible';
+            restart.style.visibility = 'hidden';
+        }
+        else if(clicks==9){
+            display.textContent='Nobody Won!';
+            playAgain.style.visibility = 'visible';
+            restart.style.visibility = 'hidden';
         }
     })
 }
@@ -112,3 +165,9 @@ function checkAngles(letter) {
 
 
 }
+
+function makeUnclickAble() {
+    for(let i = 0;i<squares.length;i++){
+        squares[i].classList.add('avoid-clicks')
+}
+};
